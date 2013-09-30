b308d6e6cce46571e0f0f1d3d374e4090ded2db5
11cfif <11cfif@gmail.com>
2013-10-01 02:53:29 +0400

    класс управления камерой

---
+++ src/ru/cfif11/cosmo/physobject/CameraControl.java
@@ -0,0 +1,11 @@
+package ru.cfif11.cosmo.physobject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Student
+ * Date: 01.10.13
+ * Time: 2:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class CameraControl {
+}
