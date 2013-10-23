06373f2e62171b964db3ebf73de08c6efa8c7f4a
11cfif <11cfif@gmail.com>
2013-10-24 01:55:54 +0400

    класс отвечающий за мышку. Немного изменена структура и архитектура довесок к предыдущему коммиту

---
+++ src/ru/cfif11/cosmo/object/physobject/HasChargeInterface.java
@@ -0,0 +1,10 @@
+package ru.cfif11.cosmo.object.physobject;
+
+
+
+/**
+ * Interface for any PhysObject3D having charge
+ */
+public interface HasChargeInterface {
+
+}
---
+++ src/ru/cfif11/cosmo/object/physobject/MassObject3D.java
@@ -0,0 +1,70 @@
+package ru.cfif11.cosmo.object.physobject;
+
+import com.threed.jpct.Object3D;
+import com.threed.jpct.SimpleVector;
+
+/**
+ * This class describes the physical objects which have mass
+ */
+public class MassObject3D extends PhysObject3D implements HasMassInterface {
+
+	private static final long serialVersionUID = -6141176990423946936L;
+	private double mass;
+
+    /**
+     * Create MassObject3D on base Object3D, velocity and mass
+     * @param obj the Object3D
+     * @param velocity the velocity
+     * @param mass the mass
+     */
+	public MassObject3D(Object3D obj, String name, SimpleVector velocity, double mass) {
+		super(obj, name, velocity);
+		this.mass = mass;
+	}
+
+    /**
+     * Returns the mass of the MassObject3D
+     * @return the mass of the MassObject3D
+     */
+	public double getMass() {
+		return mass;
+	}
+	
+	@Override
+	public void addMass(double mass) {
+		this.mass += mass;
+	}
+
+	@Override
+	public void giveMass(double mass) {
+		this.mass -= mass;
+	}
+	
+	@Override
+	public void calcAcceleration(SimpleVector force) {
+		SimpleVector gravityField = new SimpleVector(force);
+		gravityField.scalarMul((float)(1/mass));
+		if(isGravity(gravityField)) {
+			setAccelaration(gravityField);
+		} else {
+			setAcceleration(0,0,0);
+		}
+	}
+
+	@Override
+	public boolean isGravity(SimpleVector GravityField) {
+		if(GravityField.length() < 0.0005) {
+			return false;
+		}
+		return true;
+	}
+
+    /**
+     * Sets the weight of the MassObject3D
+     * @param mass the mass
+     */
+    protected void setMass(double mass) {
+        this.mass = mass;
+    }
+
+}
